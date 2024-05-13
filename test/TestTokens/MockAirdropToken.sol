3b7448beec2b0e00ddb42d36322c0909a60fcb6d
ez7212 <ez7212@gmail.com>
2024-05-13 12:07:57 -0700

    mock airdrop token

--- test/TestTokens/MockAirdropToken.sol
+++ test/TestTokens/MockAirdropToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockAirdropToken is ERC20 {
-    constructor() ERC20("Mock Airdrop Token", "Mock Airdrop") {}
+    constructor() ERC20("Mock Renzo", "MRENZO") {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
